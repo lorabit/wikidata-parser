e83c997641a2205416508b4539f1d18d61db245b
lorabit <lorabit@126.com>
2018-01-30 20:01:01 -0800

    add multithreading

--- Worker.py
+++ Worker.py
@@ -7,7 +7,7 @@
 
 
 
-# In[8]:
+# In[13]:
 
 
 import bz2
@@ -16,16 +16,16 @@ import ast
 import traceback
 from pymongo import MongoClient
 import pickle
-def consume(msg):
+def consume(thread_id, msg):
     def check_buffer(force_write = False):
         if (force_write and len(entity_buffer)>0) or len(entity_buffer)>=buffer_size:
-            print("Writing %d entities into database ..."%(len(entity_buffer)))
+            print("%s - Writing %d entities into database ..."%(thread_id,len(entity_buffer)))
             db.entities.insert_many(entity_buffer)
             entity_buffer.clear()
     #         print("%d entities writen."%(len(ids)))
 
         if (force_write and len(triplet_buffer)>0) or len(triplet_buffer)>=buffer_size:
-            print("Writing %d triplets into database ..." % (len(triplet_buffer)))
+            print("%s - Writing %d triplets into database ..." % (thread_id, len(triplet_buffer)))
             db.triplets.insert_many(triplet_buffer)
             triplet_buffer.clear()
     #         print("%d triplets writen."%(len(ids)))
@@ -107,21 +107,43 @@ def consume(msg):
     client.close()
 
 
-# In[ ]:
+# In[14]:
 
 
 import pika
-credentials = pika.PlainCredentials('wikidata', 'wikidata')
-connection = pika.BlockingConnection(pika.ConnectionParameters('10.0.0.74',5672,'/',credentials))
-channel = connection.channel()
-channel.queue_declare(queue='wikidata', durable=True)
-def callback(ch, method, properties, body):
-    consume(body)
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+def thread_worker(thread_id):
+    credentials = pika.PlainCredentials('wikidata', 'wikidata')
+    connection = pika.BlockingConnection(pika.ConnectionParameters('10.0.0.74',5672,'/',credentials))
+    channel = connection.channel()
+    channel.queue_declare(queue='wikidata', durable=True)
+    def callback(ch, method, properties, body):
+        consume(thread_id, body)
+        ch.basic_ack(delivery_tag = method.delivery_tag)
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(callback,
-                      queue='wikidata')
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(callback,
+                          queue='wikidata')
 
-channel.start_consuming()
+    print("%s start consuming." % thread_id)
+    channel.start_consuming()
+
+
+# In[15]:
+
+
+import threading
+class workerThread (threading.Thread):
+   def __init__(self, thread_id):
+      threading.Thread.__init__(self)
+      self.thread_id = thread_id
+   def run(self):
+      thread_worker(self.thread_id)
+
+threads = {}
+for thread_id in ["t1","t2","t3","t4"]:
+    threads[thread_id] = workerThread(thread_id)
+    threads[thread_id].start()
+
+for thread_id in threads:
+    threads[thread_id].join()
 
